74db1ee07282d71e74d927758e54fd0a70d888e6
Cristian Castañeda <crisshack4@gmail.com>
2024-10-01 15:21:06 -0500

    refactor player move

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -1,19 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;   
+    [SerializeField] private Rigidbody2D rb2d;
+    private Vector2 movement;
     
-    public float moveSpeed = 5f;   
-    public Rigidbody2D rb2d;
-    Vector2 movement;
-    
-    void Update()
+    private void Update()
     {
         movement.x = Input.GetAxis("Horizontal");   //movimiento x, y del player
         movement.y = Input.GetAxis("Vertical");
+        if (movement.magnitude <= 0.1f)
+        {
+            rb2d.velocity = Vector2.zero;
+        }
     }
 
     private void FixedUpdate()
