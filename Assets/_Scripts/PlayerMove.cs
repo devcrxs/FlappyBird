0e6d11dc1ad12ce3b0aea352f061b46e3f99fd22
Cristian Castañeda <crisshack4@gmail.com>
2024-10-01 15:30:43 -0500

    add joystick

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -3,12 +3,15 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;   
     [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private FloatingJoystick joystick;
     private Vector2 movement;
     
     private void Update()
     {
-        movement.x = Input.GetAxis("Horizontal");   //movimiento x, y del player
-        movement.y = Input.GetAxis("Vertical");
+        //movement.x = Input.GetAxis("Horizontal");   //movimiento x, y del player
+        //movement.y = Input.GetAxis("Vertical");
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
         if (movement.magnitude <= 0.1f)
         {
             rb2d.velocity = Vector2.zero;
