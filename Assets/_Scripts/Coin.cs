7a5163891e71ba71fd55c7ce48304debd4cb8811
JuanJo\Juan Jose <juanjrop002@gmail.com>
2024-11-01 19:47:08 +0100

    Script to get experience while getting points and expBar's funcionality

--- Assets/_Scripts/Coin.cs
+++ Assets/_Scripts/Coin.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+
+   [SerializeField] private float expEarn = 25f;
    private void OnTriggerEnter2D(Collider2D other)
    {
-      if (other.gameObject.CompareTag("Player")) return;
-      Destroy(gameObject);
+      if (other.gameObject.CompareTag("Player"))
+      {
+         BarraExp barraExp = other.GetComponent<BarraExp>();
+
+         if (barraExp != null)
+         {
+            barraExp.SetExp(expEarn); 
+         }
+         
+         Destroy(gameObject);
+         
+      }
+      
 
    }
+   
 }
