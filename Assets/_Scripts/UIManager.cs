cda8c57a95828cf59c48dfeebfdc607dec91075d
JuanJo\Juan Jose <juanjrop002@gmail.com>
2024-11-03 20:54:35 +0100

    Timer updated in UIManager

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -6,6 +6,12 @@ public class UIManager : MonoBehaviour
     [Header("Experience HUD")]
     [SerializeField] private Slider sliderBarExperience;
     [SerializeField] private TextMeshProUGUI textExperience;
+    
+    [Header("Timer")]
+    private int minutes;
+    private int seconds;
+    private float timeSpeed;
+    [SerializeField] private TextMeshProUGUI timerText;
 
     public static UIManager instance;
 
@@ -17,6 +23,12 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         UpdateExperience();
+        
+    }
+
+    private void Update()
+    {
+        UpdateTimer();
     }
 
     public void UpdateExperience()
@@ -24,5 +36,22 @@ public class UIManager : MonoBehaviour
         sliderBarExperience.value = GameManager.instance.ActualExperience / 100;
         textExperience.text = GameManager.instance.ActualLevel.ToString();
         // TODO : metodo checar nivel
+    }
+    private void UpdateTimer()
+    {
+        timeSpeed += Time.deltaTime;
+        
+        if (timeSpeed >= 1f)
+        {
+            seconds++;
+            timeSpeed = 0;
+            if (seconds == 60)
+            {
+                minutes ++;
+                seconds = 0;
+            }
+        }
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
