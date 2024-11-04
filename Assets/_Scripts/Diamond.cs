670a287bf95e9a8e1ea17b7bbc59a8879fb24117
Cristian Castaneda <crisshack4@gmail.com>
2024-11-04 17:13:36 -0500

    fix animation diamond

--- Assets/_Scripts/Diamond.cs
+++ Assets/_Scripts/Diamond.cs
@@ -7,46 +7,42 @@ public class Diamond : MonoBehaviour
     [Header("ExperienceDiamond")]
     [SerializeField] private float expEarn = 25f;
     [Header("")]
-    [SerializeField] private Transform player;
+    private Transform _player;
     [SerializeField] private float moveAwayDistance = 2f;
-    [SerializeField] private float moveAwayDuration = 0.3f;
     [SerializeField] private float followSpeed = 3f; // Velocidad de persecución
     [SerializeField] private float followDuration; // Duración de la persecución
+    private CircleCollider2D _circleCollider;
+
+    private void Start()
+    {
+        _circleCollider = GetComponent<CircleCollider2D>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
-        {
-            DiamondAnimation();
-        }
+        if (!other.CompareTag("Player")) return;
+        _circleCollider.enabled = false;
+        DiamondAnimation();
     }
 
-    public void DiamondAnimation()
+    private void DiamondAnimation()
     {
-        Vector2 directionAway = ((Vector2)transform.position - (Vector2)player.position).normalized;
+        Vector2 directionAway = ((Vector2)transform.position - (Vector2)_player.position).normalized;
         Vector2 moveAwayPosition = (Vector2)transform.position + directionAway * moveAwayDistance;
-
-        Sequence diamondSequence = DOTween.Sequence();
-
-        // Moverse lejos del jugador
-        diamondSequence.Append(transform.DOMove(moveAwayPosition, moveAwayDuration).SetEase(Ease.OutQuad));
-
-        // Al completar el movimiento, empezar a seguir al jugador
-        diamondSequence.AppendCallback(() => StartCoroutine(FollowPlayer())); // Cerrar paréntesis correctamente
+        transform.DOMove(moveAwayPosition, followDuration).OnComplete(() =>
+        {
+            StartCoroutine(FollowPlayer());
+        } );
     }
-
+    
     private IEnumerator FollowPlayer()
     {
-        float elapsedTime = 0f;
-
-        while (elapsedTime < followDuration)
+        while (Vector2.Distance(transform.position, _player.position) > 0.5f)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, followSpeed * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, _player.position, followSpeed * Time.deltaTime);
             yield return null;
         }
-
-        // Al final de la persecución, destruir el diamante y sumar experiencia
         GameManager.instance.AddExperience(expEarn);
         UIManager.instance.UpdateExperience();
         Destroy(gameObject);
